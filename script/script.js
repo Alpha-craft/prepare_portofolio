65a9365706c89d398f50b67a6101d49878a2917a
rahman <ainurahman03@gmail.com>
2020-11-27 16:46:41 +0700

    menambahkan 3 buton beserta fungsi js nya untuk memanipulasi teks dalam html

--- script/script.js
+++ script/script.js
@@ -4,18 +4,47 @@ function waktu() {
     let waktu = new Date();
     let jam = document.getElementById('jam');
     let menit = document.getElementById('menit');
-    let detik = document.getElementById('detik')
+    let detik = document.getElementById('detik');
+    let hari = document.getElementById('hari');
+    let bulan = document.getElementById('bulan');
+    let dbulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"]
 
-    let hour = jam.innerHTML = waktu.getHours();
-    let minute = menit.innerHTML = waktu.getMinutes();
-    let seconds = detik.innerHTML = waktu.getSeconds();
+    let hour = waktu.getHours();
+    let minute = waktu.getMinutes();
+    let seconds = waktu.getSeconds();
+    let day = waktu.getDate();
+    let months = waktu.getMonth();
     if (hour < 10) {
-        jam.innerHTML = '0' + waktu.getHours();
+        hour = '0' + hour;
     }
     if (minute < 10) {
-        menit.innerHTML = '0' + waktu.getHours();
+        minute = '0' + minute;
     }
     if (seconds < 10) {
-        detik.innerHTML = '0' + waktu.getHours();
+        seconds = '0' + seconds;
     }
+    let index_bulan = months;
+    detik.innerHTML = seconds;
+    jam.innerHTML = hour;
+    menit.innerHTML = minute;
+    hari.innerHTML = day;
+    bulan.innerHTML = dbulan[index_bulan]
+    console.log(index_bulan)
+        // jam.textcontent = hour + ':' + minute + ':' + seconds;
+
+}
+
+
+function ganti() {
+    let change = document.getElementById('change');
+    change.style.color = "red"
+
+}
+
+function muncul() {
+    change.style.visibility = 'visible';
+}
+
+function hilang() {
+    change.style.visibility = 'hidden';
 }
